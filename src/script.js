231d9947151ded5c82671a4b0bbc384cf01e93fc
denissea12 <denissea.12@gmail.com>
2024-09-01 17:31:04 -0500

    removed html and placed in javascript, created an array and loop to display forecast

--- src/script.js
+++ src/script.js
@@ -53,7 +53,28 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      ` 
+<div class="weather-forecast-day">
+                    <div class="weather-forecast-date">Tues</div>
+                    <div class="weather-forecast-icon">☀️</div>
+                    <div class="weather-forecast-temperatures">
+                        <div class="weather-forecast-temperature"><strong>77°</strong></div>
+                        <div class="weather-forecast-temperature"> 72°</div>
+                    </div></div>`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchformElement = document.querySelector("#search-form");
 searchformElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+displayForecast();
